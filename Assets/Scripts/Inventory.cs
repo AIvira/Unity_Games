bf6c3d1d7d6da701e03e9e20698caac1572d33a8
DESKTOP-A5I7NO7\Alexandre Bodin <alexandre.bodin78@free.fr>
2022-07-26 20:48:10 +0200

    Premiere version jeu Unity 1 - Survie - Import

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,188 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private List<ItemData> content = new List<ItemData>();
+
+    [SerializeField]
+    private GameObject inventoryPanel;
+
+    [SerializeField]
+    private Transform inventorySlotsParent;
+
+    const int InventorySize = 24;
+
+    [Header("Action Panel References")]
+
+    [SerializeField]
+    private GameObject actionPanel;
+
+    [SerializeField]
+    private GameObject useItemButton;
+
+    [SerializeField]
+    private GameObject equipItemButton;
+
+    [SerializeField]
+    private GameObject dropItemButton;
+
+    [SerializeField]
+    private GameObject destroyItemButton;
+
+    private ItemData itemCurrentlySelected;
+
+    [SerializeField]
+    private Sprite emptySlotVisual;
+
+    [SerializeField]
+    private Transform dropPoint;
+
+    [SerializeField]
+    private EquipmentLibrary equipmentLibrary;
+
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        RefreshContent();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+        }
+    }
+
+    public void AddItem(ItemData item)
+    {
+        content.Add(item);
+        RefreshContent();
+    }
+
+    public void CloseInventory()
+    {
+        inventoryPanel.SetActive(false);
+    }
+
+    private void RefreshContent()
+    {
+        for (int i = 0; i < inventorySlotsParent.childCount; i++)
+        {
+            Slot currentSlot = inventorySlotsParent.GetChild(i).GetComponent<Slot>();
+
+            currentSlot.item = null;
+            currentSlot.itemVisual.sprite = emptySlotVisual;
+        }
+
+
+        for (int i = 0; i < content.Count; i++)
+        {
+            Slot currentSlot = inventorySlotsParent.GetChild(i).GetComponent<Slot>();
+
+            currentSlot.item = content[i];
+            currentSlot.itemVisual.sprite = content[i].visual;
+        }
+    }
+
+    public bool IsFull()
+    {
+        return InventorySize == content.Count;
+    }
+    
+    public void OpenActionPanel(ItemData item, Vector3 slotPosition)
+    {
+        itemCurrentlySelected = item;
+
+        if(item == null)
+        {
+            actionPanel.SetActive(false);
+            return;
+        }
+
+        switch(item.itemType)
+        {
+            case ItemType.Ressource:
+                useItemButton.SetActive(false);
+                equipItemButton.SetActive(false); 
+                break;
+            
+            case ItemType.Equipment:
+                useItemButton.SetActive(false);
+                equipItemButton.SetActive(true); 
+                break;
+
+            case ItemType.Consumable:
+                useItemButton.SetActive(true);
+                equipItemButton.SetActive(false); 
+                break;
+        }
+
+        actionPanel.transform.position = slotPosition;
+        actionPanel.SetActive(true);
+    }
+
+    public void CloseActionPanel()
+    {
+        actionPanel.SetActive(false);
+        itemCurrentlySelected = null;
+    }
+
+    public void UseActionButton()
+    {
+        print("Use item : " + itemCurrentlySelected.name);
+        CloseActionPanel();
+
+    }
+
+    public void EquipActionButton()
+    {
+        print("Equip item : " + itemCurrentlySelected.name);
+
+        EquipmentLibraryItem equipmentLibraryItem = equipmentLibrary.content.Where(elem => elem.itemData == itemCurrentlySelected).First();
+
+        if(equipmentLibraryItem != null)
+        {
+            for (int i = 0; i < equipmentLibraryItem.elementsToDisable.Length; i++)
+            {
+                equipmentLibraryItem.elementsToDisable[i].SetActive(false);
+            }
+
+            equipmentLibraryItem.itemPrefab.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Equipement : " + itemCurrentlySelected.name + "non existant dans la librairie des équipements");
+        }
+
+        CloseActionPanel();
+
+    }
+
+    public void DropActionButton()
+    {
+        GameObject instantiatedItem = Instantiate(itemCurrentlySelected.prefab);
+        instantiatedItem.transform.position = dropPoint.position;
+        content.Remove(itemCurrentlySelected);
+        CloseActionPanel();
+        RefreshContent();
+    }
+
+    public void DestroyActionButton()
+    {
+        content.Remove(itemCurrentlySelected);
+        CloseActionPanel();
+        RefreshContent();
+    }
+}
